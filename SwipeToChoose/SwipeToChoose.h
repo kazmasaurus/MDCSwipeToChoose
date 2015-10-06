670987c2501840a5c87e46038ae74bb27497c3b1
Zak Remer <zak.remer@gmail.com>
2015-10-06 11:12:07 -0700

    Shares scheme.

---
+++ SwipeToChoose/SwipeToChoose.h
@@ -0,0 +1,19 @@
+//
+//  SwipeToChoose.h
+//  SwipeToChoose
+//
+//  Created by Zak Remer on 10/6/15.
+//  Copyright © 2015 modocache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeToChoose.
+FOUNDATION_EXPORT double SwipeToChooseVersionNumber;
+
+//! Project version string for SwipeToChoose.
+FOUNDATION_EXPORT const unsigned char SwipeToChooseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeToChoose/PublicHeader.h>
+
+
